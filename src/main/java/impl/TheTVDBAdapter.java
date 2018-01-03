dd4c378e8d79dca14d573001a7bb336ea0214182
Adrian Pic <al315544@uji.es>
2018-01-03 15:57:52 +0100

    Corriendo errores de build.

--- src/main/java/impl/TheTVDBAdapter.java
+++ src/main/java/impl/TheTVDBAdapter.java
@@ -2,6 +2,7 @@ package impl;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -21,80 +22,80 @@ import impl.model.RemoteSerie;
 
 public class TheTVDBAdapter implements IRemoteManager {
 
-	private static final String URI = "https://api.thetvdb.com";
-
-	private static URI getBaseURI() {
-		return UriBuilder.fromUri(URI).build();
-	}
-	
-	// ------------------------------------------------------------------------
-
-	private Client client;
-	private JsonFactory jsonFactory;
-	private Token token;
-
-	public TheTVDBAdapter() {
-		client = ClientBuilder.newClient();
-		jsonFactory = new JsonFactory();
-		this.setToken();
-	}
-	
-	// ------------------------------------------------------------------------
-
-	@Override
-	public Map<Long, String> searchSeries(String pattern) {
-		Map<Long, String> theTVDBSeries = new TreeMap<>();
-
-		Response response = client
-				.target(getBaseURI())
-				.path("search")
-				.path("series")
-				.queryParam("name", pattern)
-				.request(MediaType.APPLICATION_JSON)
-				.header("Authorization", "Bearer " + token.getToken())
-				.header("Accept-Language", "en")
-				.get();
-
-		try {
-			JsonParser jsonParser = jsonFactory.createParser(response.readEntity(String.class));
-			
-			String idSerie = null;
-			String serieName = null;
-			
-			while (!jsonParser.isClosed()) {
-				JsonToken jsonToken = jsonParser.nextToken();
-				
-				if (JsonToken.FIELD_NAME.equals(jsonToken)) {
-					
-					if (jsonParser.getCurrentName().equals("id")) {
-						jsonParser.nextToken();
-						idSerie = jsonParser.getValueAsString();
-					}
-					
-					if (jsonParser.getCurrentName().equals("seriesName")) {
-						jsonParser.nextToken();
-						serieName = jsonParser.getValueAsString();
-					}
-					
-					if (idSerie != null && serieName != null) {
-						theTVDBSeries.put(Long.parseLong(idSerie), serieName);
-						idSerie = null;
-						serieName = null;
-					}
-
-				}
-
-			}
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		response.close();
-
-		return theTVDBSeries;
-	}
-	
+//	private static final String URI = "https://api.thetvdb.com";
+//
+//	private static URI getBaseURI() {
+//		return UriBuilder.fromUri(URI).build();
+//	}
+//	
+//	// ------------------------------------------------------------------------
+//
+//	private Client client;
+//	private JsonFactory jsonFactory;
+//	private Token token;
+//
+//	public TheTVDBAdapter() {
+//		client = ClientBuilder.newClient();
+//		jsonFactory = new JsonFactory();
+//		this.setToken();
+//	}
+//	
+//	// ------------------------------------------------------------------------
+//
+//	@Override
+//	public List<RemoteSearchSerie> searchSeries(String pattern) {
+//		Map<Long, String> theTVDBSeries = new TreeMap<>();
+//
+//		Response response = client
+//				.target(getBaseURI())
+//				.path("search")
+//				.path("series")
+//				.queryParam("name", pattern)
+//				.request(MediaType.APPLICATION_JSON)
+//				.header("Authorization", "Bearer " + token.getToken())
+//				.header("Accept-Language", "en")
+//				.get();
+//
+//		try {
+//			JsonParser jsonParser = jsonFactory.createParser(response.readEntity(String.class));
+//			
+//			String idSerie = null;
+//			String serieName = null;
+//			
+//			while (!jsonParser.isClosed()) {
+//				JsonToken jsonToken = jsonParser.nextToken();
+//				
+//				if (JsonToken.FIELD_NAME.equals(jsonToken)) {
+//					
+//					if (jsonParser.getCurrentName().equals("id")) {
+//						jsonParser.nextToken();
+//						idSerie = jsonParser.getValueAsString();
+//					}
+//					
+//					if (jsonParser.getCurrentName().equals("seriesName")) {
+//						jsonParser.nextToken();
+//						serieName = jsonParser.getValueAsString();
+//					}
+//					
+//					if (idSerie != null && serieName != null) {
+//						theTVDBSeries.put(Long.parseLong(idSerie), serieName);
+//						idSerie = null;
+//						serieName = null;
+//					}
+//
+//				}
+//
+//			}
+//
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//
+//		response.close();
+//
+//		return theTVDBSeries;
+//	}
+//	
 //	public Serie getSerie(long id) {
 //		Serie serie = new Serie();
 //		Map<String, String> theTVDBSeriefields = new HashMap<>();
@@ -141,34 +142,46 @@ public class TheTVDBAdapter implements IRemoteManager {
 //
 //		return serie;
 //	}
-
-	private void setToken() {
-		Response response = client
-				.target(getBaseURI())
-				.path("login")
-				.request(MediaType.APPLICATION_JSON)
-				.post(Entity.json(new Login()));
-		
-		token = response.readEntity(Token.class);
-		response.close();
-	}
-	
-	// ------------------------------------------------------------------------
-
-	public static void main(String[] args) {
-		TheTVDBAdapter adapter = new TheTVDBAdapter();
-		System.out.println(adapter.searchSeries("The OA").values());
-		//System.out.println(adapter.getSerie(321060));
-	}
+//
+//	private void setToken() {
+//		Response response = client
+//				.target(getBaseURI())
+//				.path("login")
+//				.request(MediaType.APPLICATION_JSON)
+//				.post(Entity.json(new Login()));
+//		
+//		token = response.readEntity(Token.class);
+//		response.close();
+//	}
+//	
+//	// ------------------------------------------------------------------------
+//
+//	public static void main(String[] args) {
+//		TheTVDBAdapter adapter = new TheTVDBAdapter();
+//		System.out.println(adapter.searchSeries("The OA").values());
+//		//System.out.println(adapter.getSerie(321060));
+//	}
+//
+//	@Override
+//	public RemoteSerie getSerie(long codSerie) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
 
 	@Override
-	public RemoteSerie getSerie(long codSerie) {
+	public Map<String, String> getSeason(long codSeason) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Map<String, String> getSeason(long codSeason) {
+	public List<RemoteSerie> searchSeries(String pattern) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public RemoteSerie getSerie(long codSerie) {
 		// TODO Auto-generated method stub
 		return null;
 	}
