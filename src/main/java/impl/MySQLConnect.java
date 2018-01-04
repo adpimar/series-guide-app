cbf8616275b2d17a835d75229b9a7c6784d2e7c4
al152090 <al152090@uji.es>
2018-01-04 19:39:43 +0100

    Modificada MySQLConnect

--- src/main/java/impl/MySQLConnect.java
+++ src/main/java/impl/MySQLConnect.java
@@ -5,24 +5,32 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MySQLConnect {
-
-	public static void main(String[] args) {
-		 
-        //Creates the Connection object
-        Connection conn = null;
- 
-        try {
-            String url = "jdbc:mysql://mysql:3306/seriesguideapp-adan";
- 
-            conn = DriverManager.getConnection(url);
-            if (conn != null) {
-                System.out.println("Connected to the database seriesguideapp-adan");
-            }
- 
-        } catch (SQLException ex) {
+	
+	static final String url = "jdbc:mysql://mysql:3306/seriesguideapp-adan";
+	
+	//Creates the Connection object
+    static Connection connection = null;
+    
+	public static Connection getConnection() {
+		
+		try {
+			if (connection != null && !connection.isClosed())
+				return connection;
+			
+			connection = DriverManager.getConnection(url);
+			
+			if (connection != null) 
+				System.out.println("Connected to the database seriesguideapp-adan");
+			else 
+				System.out.println("Connexion to the database seriesguideapp-adan has failed!");
+			
+		} catch (SQLException ex) {
             System.out.println("An error occurred. <MySQLConnect>");
             ex.printStackTrace();
         }
-    }
+		
+		return connection;
+	
+	}
 	
 }
