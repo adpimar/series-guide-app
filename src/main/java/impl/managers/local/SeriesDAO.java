6cf2894a19a4cac0027072fba69d9a7f601a00db
Adrian Pic <al315544@uji.es>
2018-01-10 22:51:07 +0100

    Reescribiendo SeriesDAO.
    Separada la parte que necesita los parsers de SQL de la parte del
    'test'. Ahora es una clase en la parte 'test' que extiende SeriesDAO.

--- src/main/java/impl/managers/local/SeriesDAO.java
+++ src/main/java/impl/managers/local/SeriesDAO.java
@@ -1,9 +1,5 @@
 package impl.managers.local;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -16,70 +12,10 @@ import abs.managers.ILocalManager;
 import impl.model.Episode;
 import impl.model.Season;
 import impl.model.Serie;
-import resources.ParserSQL;
 
 public class SeriesDAO implements ILocalManager {
 
 	private static final int OVERVIEW_LIMIT = 500;
-
-	public SeriesDAO() {
-
-	}
-
-	public SeriesDAO(String filename) {
-		configureBDL(filename);
-	}
-
-	// ------------------------------------------------------------------------
-
-	private void configureBDL(String filename) {
-		MySQLConnect conexion = new MySQLConnect();
-		Statement stm = null;
-		BufferedReader b;
-		String line = "";
-		String sqlSentence = "";
-		Connection connect;
-
-		try {
-
-			connect = conexion.getConnection();
-			stm = connect.createStatement();
-
-			stm.execute("DELETE FROM episodios");
-			stm.execute("DELETE FROM temporadas");
-			stm.execute("DELETE FROM series");
-
-			b = new BufferedReader(new FileReader(filename));
-
-			while ((line = b.readLine()) != null) {
-				String[] fields = line.split("#");
-				switch (fields[0].charAt(0)) {
-				case 'S':
-					sqlSentence = ParserSQL.serieInsertSQL(fields);
-					break;
-				case 'T':
-					sqlSentence = ParserSQL.seasonInsertSQL(fields);
-					break;
-				case 'E':
-					sqlSentence = ParserSQL.episodeInsertSQL(fields);
-				}
-				stm.execute(sqlSentence);
-			}
-
-			stm.close();
-			connect.close();
-			b.close();
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-			System.out.println(sqlSentence);
-		}
-
-	}
 
 	// ------------------------------------------------------------------------
 
